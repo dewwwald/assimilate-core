63f7cf5fdb45f15a6940ce968c24328100a2c548
Dewald Laubscher <laubscher.dewald@gmail.com>
2018-04-10 23:05:43 -0400

    document code

--- Provider/Provider.js
+++ Provider/Provider.js
@@ -1,8 +1,10 @@
 module.exports = class Provider {
+    /**
+     * @var Container@epitome-core/Container
+     */
     get container() {
         return this._container;
     }
-
     set container(value) {
         Object.defineProperty(this, '_container', {
             writable: false, value: value
@@ -13,11 +15,21 @@ module.exports = class Provider {
         this.container = container;
     }
 
+    /**
+     * Initializes the service provider, callback must be called
+     * @param function next() {}
+     */
     initialize(next) {
         next();
     }
 
+    /**
+     * Step 1 registers the service provider
+     */
     register() { }
 
+    /**
+     * Step 3 finalizes the service provider 
+     */
     final() { }
 }
