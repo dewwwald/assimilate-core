e8210cd4b2fdf45834d5c17c0405e2c603cd5d81
Dewald Laubscher <laubscher.dewald@gmail.com>
2018-04-11 22:53:25 -0400

    cleanup

--- Provider/SeederProvider.js
+++ Provider/SeederProvider.js
@@ -8,22 +8,20 @@ module.exports = class SeederProvider extends Provider {
         seederTable.get().then(() => {
             const ignoreList = seederTable.data.map(x => x.name + '.js');
             const seedFileList = fs.readdirSync(path.resolve('./Seeds'));
-            seedFileList.forEach(filename => this.handleSeeds(filename, ignoreList));
+            seedFileList.forEach(filename => this._handleSeeds(filename, ignoreList));
         });
     }
 
-    handleSeeds(fileName, ignoreList) {
+    _handleSeeds(fileName, ignoreList) {
         if (!ignoreList.includes(fileName)) {
             const Seeding = require(path.resolve('./Seeds/' + fileName));
             const seeding = new Seeding(this.container);
-            seeding.insert().then((items) => {
-                console.log(typeof items);
+            seeding.insert().then(items => {
                 const Seeder = this.container.make('Model/Seeder', {
                     name: fileName.replace('.js', ''),
                     target: seeding.list,
                     state: typeof items.length !== 'undefined' ? items.map(x => x._id) : [items]
                 });
-                console.log(Seeder.data);
                 Seeder.save();
             });
         }
